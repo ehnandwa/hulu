ac522d9538c875c92cc2afe173659d0ae992cd3f
ehnandwa <nandwaharry24@gmail.com>
2021-07-14 00:10:32 +0300

    test

--- components/Header.js
+++ components/Header.js
@@ -13,16 +13,16 @@ function Header() {
     return(
         <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
             <div className="flex flex-grow justify-evenly max-w-2xl">
-                <HeaderItem title='HOME' Icon={HomeIcon}/>
-                <HeaderItem title='TRENDING' Icon={LightningBoltIcon}/>
-                <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon}/>
-                <HeaderItem title='COLLECTIONS' Icon={CollectionIcon}/>
-                <HeaderItem title='SEARCH' Icon={SearchIcon}/>
-                <HeaderItem title='ACCOUNT' Icon={UserIcon}/>
+                <HeaderItem title='home' Icon={HomeIcon}/>
+                <HeaderItem title='trending' Icon={LightningBoltIcon}/>
+                <HeaderItem title='verified' Icon={BadgeCheckIcon}/>
+                <HeaderItem title='collections' Icon={CollectionIcon}/>
+                <HeaderItem title='search' Icon={SearchIcon}/>
+                <HeaderItem title='account' Icon={UserIcon}/>
             </div>
             <Image src="https://links.papareact.com/ua6"
                    className="object-contain"
-                   width={200} height={100}/>
+                   width={200} height={100} alt=""/>
         </header>
     )
 }
